3369b314c9fb7d9f92a70234a0ef9e719a71eefb
arzuakov@yahoo.com <arzuakov@yahoo.com>
2024-02-22 16:30:42 +0300

    w22.02.2024

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
+
+	@HttpCode(HttpStatus.OK)
+	@Post('login')
+	signIn(@Body() signInDto: Record<string, any>) {
+		return this.authService.signIn(signInDto.username, signInDto.password)
+	}
 
 	@Get()
 	getHello(): string {
