d9452e2bd8bb17177532056f73c196ae01f0e5e0
arzuakov@yahoo.com <arzuakov@yahoo.com>
2024-04-23 17:37:49 +0300

    w23.04.2024

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
+
+	@Get('hello')
+	hello() {
+		return 'hello world'
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async login(@Body() dto: AuthDto) {
+		return this.authService.login(dto)
+	}
 }
