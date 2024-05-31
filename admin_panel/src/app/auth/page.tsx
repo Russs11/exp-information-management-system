82299166813641325177b771cdd7af368ea068f7
Russs11 <russs.khalilov@gmail.com>
2024-06-01 01:32:16 +0300

    h01.06.2024

---
+++ admin_panel/src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next'
+import { Auth } from './Auth'
+
+
+
+export const metadata: Metadata = {
+	title: 'Auth'
+}
+
+export default function AuthPage(){
+	return <Auth/>
+} 
