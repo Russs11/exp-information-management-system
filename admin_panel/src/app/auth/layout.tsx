9eba2941b4bd55f590c90fcc70404673ab0a60ba
Russs11 <russs.khalilov@gmail.com>
2024-06-04 17:33:23 +0300

    w04.06.2024

---
+++ admin_panel/src/app/auth/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Auth',
+  description: 'authentication',
+}
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  )
+}
