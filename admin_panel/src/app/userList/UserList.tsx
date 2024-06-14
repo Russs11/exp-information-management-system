b0b73112995bb1bdee50b41b9d2f1188a27ca989
Russs11 <russs.khalilov@gmail.com>
2024-06-14 17:21:34 +0300

    w14.06.2024

--- admin_panel/src/app/userList/UserList.tsx
+++ admin_panel/src/app/userList/UserList.tsx
@@ -1,6 +1,33 @@
 'use client'
 
+import Link from 'next/link'
+import Image from 'next/image'
+import { UserThread } from './components/userThread'
 export function UserList() {
+
+	interface IUser{
+		name: string,
+		id:number
+	}
+
+	let threadsArr: JSX.Element[] = []
+
+	let userArray: IUser[] = [
+    { name: 'Ivanov', id: 1 },
+		{ name: 'Petrov', id: 2 },
+		{ name: 'Sidorov', id: 3 },
+		
+  ]
+
+	let userArray2 = new Array(30).fill({ name: 'Ivanov', id: 1 })
+	console.log(userArray2)
+
+	threadsArr = userArray2.map((item, index) => {
+    return <UserThread key={index + 1} />
+  })
+
+
+
   return (
     <div className='flex-auto justify-center p-0 overflow-auto h-screen md:p-12'>
       <div className='relative flex flex-col w-full mb-0 break-words bg-white border-0 border-transparent border-solid shadow-xl rounded-2xl bg-clip-border min-w-fit'>
@@ -28,14 +55,16 @@ export function UserList() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                {/* <tr>
                   <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
                     <div className='flex px-2 py-1'>
                       <div>
-                        <img
-                          src='./image/Ivanov.jpg'
+                        <Image
+                          src='/Petrov_cr.jpg'
                           className='inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-in-out text-sm h-9 w-9 rounded-xl'
                           alt='user1'
+                          width={36}
+                          height={36}
                         />
                       </div>
                       <div className='flex flex-col justify-center'>
@@ -70,168 +99,16 @@ export function UserList() {
                     </span>
                   </td>
                   <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <a
+                    <Link
                       href='javascript:;'
                       className='font-semibold leading-tight text-xs text-slate-400'
                     >
                       {' '}
                       Редакт.{' '}
-                    </a>
+                    </Link>
                   </td>
-                </tr>
-                <tr>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <div className='flex px-2 py-1'>
-                      <div>
-                        <img
-                          src='./image/Sidorov.jpg'
-                          className='inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-in-out text-sm h-9 w-9 rounded-xl'
-                          alt='user2'
-                        />
-                      </div>
-                      <div className='flex flex-col justify-center'>
-                        <h6 className='mb-0 leading-normal text-sm'>
-                          Сидоров Пётр
-                        </h6>
-                        <p className='mb-0 leading-tight text-xs text-slate-400'>
-                          Петрович
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <p className='mb-0 font-semibold leading-tight text-xs'>
-                      ОП №2 Киевский
-                    </p>
-                    <p className='mb-0 leading-tight text-xs text-slate-400'>
-                      Старший эксперт
-                    </p>
-                  </td>
-                  <td className='p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent'>
-                    <span className='bg-gradient-to-tl from-slate-600 to-slate-300 px-3.6 text-xs rounded-1.8 py-2.2 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white'>
-                      Offline
-                    </span>
-                  </td>
-                  <td className='p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      23.04.24
-                    </span>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      в 16:30
-                    </span>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <a
-                      href='javascript:;'
-                      className='font-semibold leading-tight text-xs text-slate-400'
-                    >
-                      {' '}
-                      Редакт{' '}
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <div className='flex px-2 py-1'>
-                      <div>
-                        <img
-                          src='./image/Petrov_cr.jpg'
-                          className='inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-in-out text-sm h-9 w-9 rounded-xl'
-                          alt='user1'
-                        />
-                      </div>
-                      <div className='flex flex-col justify-center'>
-                        <h6 className='mb-0 leading-normal text-sm'>
-                          Петров Иван
-                        </h6>
-                        <p className='mb-0 leading-tight text-xs text-slate-400'>
-                          Сергеевич
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <p className='mb-0 font-semibold leading-tight text-xs'>
-                      ОМВД по Бахчисарайскому р-ну
-                    </p>
-                    <p className='mb-0 leading-tight text-xs text-slate-400'>
-                      Эксперт
-                    </p>
-                  </td>
-                  <td className='p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent'>
-                    <span className='bg-gradient-to-tl from-emerald-500 to-teal-400 px-3.6 text-xs rounded-1.8 py-2.2 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white'>
-                      Online
-                    </span>
-                  </td>
-                  <td className='p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      23.04.24
-                    </span>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      в 20:45
-                    </span>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <a
-                      href='javascript:;'
-                      className='font-semibold leading-tight text-xs text-slate-400'
-                    >
-                      {' '}
-                      Редакт.{' '}
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <div className='flex px-2 py-1'>
-                      <div>
-                        <img
-                          src='./image/Petrova.jpg'
-                          className='inline-flex items-center justify-center mr-4 text-white transition-all duration-200 ease-in-out text-sm h-9 w-9 rounded-xl'
-                          alt='user1'
-                        />
-                      </div>
-                      <div className='flex flex-col justify-center'>
-                        <h6 className='mb-0 leading-normal text-sm'>
-                          Петрова Светлана
-                        </h6>
-                        <p className='mb-0 leading-tight text-xs text-slate-400'>
-                          Петровна
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <p className='mb-0 font-semibold leading-tight text-xs'>
-                      ОМВД по Евпатории
-                    </p>
-                    <p className='mb-0 leading-tight text-xs text-slate-400'>
-                      Эксперт
-                    </p>
-                  </td>
-                  <td className='p-2 leading-normal text-center align-middle bg-transparent border-b text-sm whitespace-nowrap shadow-transparent'>
-                    <span className='bg-gradient-to-tl from-emerald-500 to-teal-400 px-3.6 text-xs rounded-1.8 py-2.2 inline-block whitespace-nowrap text-center align-baseline font-bold uppercase leading-none text-white'>
-                      Online
-                    </span>
-                  </td>
-                  <td className='p-2 text-center align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      23.04.24
-                    </span>
-                    <span className='font-semibold leading-tight text-xs text-slate-400'>
-                      в 00:05
-                    </span>
-                  </td>
-                  <td className='p-2 align-middle bg-transparent border-b whitespace-nowrap shadow-transparent'>
-                    <a
-                      href='javascript:;'
-                      className='font-semibold leading-tight text-xs text-slate-400'
-                    >
-                      {' '}
-                      Редакт.{' '}
-                    </a>
-                  </td>
-                </tr>
+                </tr> */}
+								{threadsArr}
               </tbody>
             </table>
           </div>
