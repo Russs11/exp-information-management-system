35d830cb4a5e8d25288bed6f2eb54e588188b6b0
Russs11 <russs.khalilov@gmail.com>
2024-07-26 17:59:13 +0300

    w26.07.2024

---
+++ admin_panel/src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { EnumTokens } from './services/auth-token.service'
+
+export async function middleware(request: NextRequest, response: NextResponse) {
+	const { url, cookies } = request
+	
+	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+
+
+const isDashboardPage = url.includes('/userList')
+const isAuthPage = url.includes('/auth')
+
+	if (isAuthPage && refreshToken) {
+		return NextResponse.redirect(new URL('/userList', url))
+	}
+	if (isAuthPage) {
+		return NextResponse.next()
+	}
+	if (!refreshToken) {
+		return NextResponse.redirect(new URL('/auth', request.url))
+	}
+	return NextResponse.next()
+}
+
