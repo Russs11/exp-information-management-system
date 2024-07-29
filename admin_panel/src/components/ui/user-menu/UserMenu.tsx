df8eda7724b7829b13f338386f1ab6e43920beaa
Russs11 <russs.khalilov@gmail.com>
2024-07-29 14:33:32 +0300

    w29.07.2024

--- admin_panel/src/components/ui/user-menu/UserMenu.tsx
+++ admin_panel/src/components/ui/user-menu/UserMenu.tsx
@@ -1,42 +1,57 @@
-import Link from 'next/link'
+import { authService } from '@/services/auth.service'
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { DetailedHTMLProps, HTMLAttributes } from 'react'
-
+import { Button } from '../buttons/Button'
 interface IUserMenuProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 
+export const UserMenu = ({ ...props }: IUserMenuProps): JSX.Element => {
 
-	export const UserMenu = ({
-    ...props
-  }: IUserMenuProps): JSX.Element => {
-    return (
-      <div
-        className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
-        role='menu'
-        aria-orientation='vertical'
-        aria-labelledby='user-menu-button'
+  const {push} = useRouter()
+
+  const { mutate } = useMutation({
+    mutationKey: ['logout'],
+    mutationFn: () => authService.logout(),
+    onSuccess() {
+      push('/auth')
+  
+    },
+  })
+
+
+  return (
+    <div
+      className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+      role='menu'
+      aria-orientation='vertical'
+      aria-labelledby='user-menu-button'
+      tabIndex={-1}
+    >
+      <a
+        href='#'
+        className='block px-4 py-2 text-sm text-gray-700'
+        role='menuitem'
         tabIndex={-1}
+        id='user-menu-item-0'
       >
-        <a
-          href='#'
-          className='block px-4 py-2 text-sm text-gray-700'
-          role='menuitem'
-          tabIndex={-1}
-          id='user-menu-item-0'
-        >
-          Профиль
-        </a>
-        <a
-          href='#'
-          className='block px-4 py-2 text-sm text-gray-700'
-          role='menuitem'
-          tabIndex={-1}
-          id='user-menu-item-1'
-        >
-          Настройки профиля
-        </a>
-        <Link href='/auth' className='block px-4 py-2 text-sm text-gray-700'>
-          Выйти
-        </Link>
-      </div>
-    )
-  }
+        Профиль
+      </a>
+      <a
+        href='#'
+        className='block px-4 py-2 text-sm text-gray-700'
+        role='menuitem'
+        tabIndex={-1}
+        id='user-menu-item-1'
+      >
+        Настройки профиля
+      </a>
+      <Button
+        className='block px-4 py-2 text-sm text-gray-700'
+        onClick={() => mutate()}
+      >
+        Выйти
+      </Button>
+    </div>
+  )
+}
