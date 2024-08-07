aaee5457fc73d17e9e269bddd21f1614fa5994d2
Russs11 <russs.khalilov@gmail.com>
2024-08-08 01:12:07 +0300

    added role check h08.08.2024

--- admin_panel/src/services/auth.service.ts
+++ admin_panel/src/services/auth.service.ts
@@ -1,11 +1,17 @@
 import { axiosClassic } from '@/api/interceptors'
 import { IAuthForm, IAuthResponse } from '@/types/auth.types'
+import { NextResponse } from 'next/server'
 import { removeFromStorage, saveTokenStorage } from './auth-token.service'
 
 export const authService = {
   async main(data: IAuthForm) {
     const response = await axiosClassic.post<IAuthResponse>('/auth/login', data)
 
+    if (response.data.user.role !== 'user') {
+      alert('Недостоточно прав для входа в админ панель')
+      return NextResponse.redirect('/auth')
+    }
+    
     if (response.data.accessToken) saveTokenStorage(response.data.accessToken)
     return response
   },
