37f6332c2caddb6561ca20ec0e3bb6a5f16dc3dd
Russs11 <russs.khalilov@gmail.com>
2024-11-07 23:54:59 +0300

    h07.11.2024

--- admin_panel/src/app/i/userList/components/userThread.tsx
+++ admin_panel/src/app/i/userList/components/userThread.tsx
@@ -81,7 +81,7 @@ export function UserThread({ name, role, updateAt, id }: IUserThread) {
         </Link>
         <td className='p-2 text-center align-middle bg-transparent whitespace-nowrap shadow-transparent'>
           <Button
-            type={'submit'}
+            // type={'submit'}
             className='rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'
             onClick={() => mutate({ id })}
           />
