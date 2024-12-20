145d6f60913d658bd37c87bb51dc0740b54bbd5b
Russs11 <russs.khalilov@gmail.com>
2024-12-20 14:42:58 +0300

    w20.12.2024

--- admin_panel/src/app/i/userList/UserList.tsx
+++ admin_panel/src/app/i/userList/UserList.tsx
@@ -35,18 +35,14 @@ export function UserList() {
       <div className='fixed z-10 left-5 py-5'>
         <SheetDemo />
       </div>
-     
       <div className='flex flex-col w-full break-words bg-white border-0 border-transparent border-solid shadow-xl rounded-2xl bg-clip-border min-w-fit mt-5'>
-
-
         <div className='p-6 pb-0 mb-0 bg-white rounded-t-2xl'>
           <h6>Список пользователей</h6>
         </div>
         <div className='flex-auto px-0 pt-0 pb-2'>
           <div className='p-0'>
             <table className='items-center w-full mb-0 align-top border-gray-200 text-slate-500'>
-              
-              <thead className='align-bottom'>
+              <thead className='align-bottom '>
                 <tr>
                   <th className='px-6 py-3 font-bold text-left uppercase align-middle bg-transparent border-b border-gray-200 shadow-none text-xxs border-b-solid tracking-none whitespace-nowrap text-slate-400 opacity-70'>
                     Пользователь
